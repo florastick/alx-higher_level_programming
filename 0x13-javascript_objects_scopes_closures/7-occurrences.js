27781ae128a1cccc14752f4b5b24964499b35a93
John Evwer <119025958+florastick@users.noreply.github.com>
2023-06-17 15:04:21 +0100

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,6 @@
+#!/usr/bin/node
 
-rts.nbOccurences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let count = 0;
 
   for (let i = 0; i < list.length; i++) {
